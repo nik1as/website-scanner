2f78f8c923ba2416a8d31a618ab1a460af800d42
Niklas Oberender <mail@niklas-oberender.de>
2025-04-18 12:51:05 +0200

    improve email regex

--- website_scanner/regex.py
+++ website_scanner/regex.py
@@ -2,5 +2,4 @@ import re
 
 HTML_COMMENT_REGEX = re.compile(r"<!--(.*?)-->", re.DOTALL)
 EMAIL_REGEX = re.compile(
-    r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-HTTP_URL_REGEX = re.compile(r"https?://")
+    r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z]{2,}", re.IGNORECASE)
